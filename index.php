b81fca36730c0192055679e6625484e20e169469
motomotoyoshi <motomoto.git@gmail.com>
2019-09-04 02:27:50 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Poll</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Which do you like?</h1>
+    <form action="" method="post">
+        <div class="row">
+            <div class="box" id="box_0" data-id="0"></div>
+            <div class="box" id="box_1" data-id="1"></div>
+            <div class="box" id="box_2" data-id="2"></div>
+        </div>
+        <div id="btn">Vote and See Results</div>
+    </form>
+</body>
+</html>

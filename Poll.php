38b75214227e636700a9efdca48e85c5e759964d
motomotoyoshi <motomoto.git@gmail.com>
2019-09-19 02:27:00 +0900

    update, Poll.php

--- Poll.php
+++ Poll.php
@@ -72,12 +72,17 @@ class Poll {
 
     private function _save() {
         $sql = "insert into answers
-                    (answer, created)
-                values (:answer, now())";
+                    (answer, created, remote_addr, user_agent, answer_date)
+                values (:answer, now(), :remote_addr, :user_agent, now())";
         $stmt = $this->_db->prepare($sql);
         $stmt->bindValue(':answer', (int)$_POST['answer'], \PDO::PARAM_INT);
-        $stmt->execute();
-        // exit;
+        $stmt->bindValue(':remote_addr', $_SERVER['REMOTE_ADDR'], \PDO::PARAM_STR);
+        $stmt->bindValue(':user_agent', $_SERVER['HTTP_USER_AGENT'], \PDO::PARAM_STR);
+        try {
+            $stmt->execute();
+        }catch(\PDOException $e){
+            throw new \Exception('No more vote for today!');
+        }
     }
 
     private function _connectDB() {
